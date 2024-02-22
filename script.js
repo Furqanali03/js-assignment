9a0f62667d29bff53f496531b1def9a571a553bd
Furqan Ali <131483622+Furqanali03@users.noreply.github.com>
2024-02-22 18:19:30 +0500

    js assignment

---
+++ script.js
@@ -0,0 +1,101 @@
+// variabls
+
+// var firstName = "Furqan"
+// var lastName = "Ali"
+// var age = 23
+
+// console.log("my name is"  + " " + firstName  +" "+ lastName +" "+ "and age is"  + " "+ age)
+
+
+// alert
+// alert("Error, Please enter a valid password!");
+
+// alert("Welcome to JS Land.... \n Happy Coding!")
+
+// confirm
+// confirm("Please enter your age:");
+
+// console.log
+// console.log("Happy Coding!");
+
+
+// prompt function
+// var a = prompt("Enter your number")
+// console.log("Your number is" +" "+ a)
+
+
+
+// javascript variable
+
+// console.log("Assalam 0 Alikum Every one!")
+
+
+// var a = 5; //number  
+// var b = 6; //number 
+// var c = "Furqan"; //string
+
+
+// console.log(a + b + 8)
+
+// console.log(typeof(c))
+
+// console.log(typeof a, typeof b,typeof c)
+
+
+// variable for string 
+
+// var a = prompt("Enter your name!")
+// alert(a)
+
+
+// var a = prompt("Enter your age!")
+// alert("Your age is"+ " " + a)
+
+
+// confirm("Certified Mobile Application Development")
+
+
+// alert("Pizza \nPizz \nPiz \nPi \nP");
+
+
+// Email address
+
+// var a = prompt("Enter your email address");
+// alert("Your email address is"+" "+ a);
+
+
+// alert 
+// alert("I am trying to learn from book \nsmarter way to learn javascript!")
+
+// // Storing symbols in variables as strings
+// var symbol1 = "*";
+// var symbol2 = "$";
+
+// // Alert with symbols
+// alert("________________"+ symbol1  + symbol2 + symbol1 + "________________");
+
+
+
+
+// Variable for numbers 
+
+// var a = prompt("Enter your age!");
+// alert("I am "+a+" years old.");
+
+
+// var a = 14;
+// alert("You have visited "+ a +" times this website!")
+
+
+// var a = prompt("Enter your date of birth!")
+
+// console.log("My birth year is " + a );
+// console.log(typeof a);
+
+
+var vistorName = "Ali";
+var quantity = "5 T-shirts";
+var website = "xyz.com";
+
+console.log(vistorName + " have ordered " + quantity + " on " + website)
+
